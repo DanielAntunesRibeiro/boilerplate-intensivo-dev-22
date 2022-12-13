6f0f76e5b5847498261c94f9335f6b156574ef74
DanielAntunesRibeiro <d.a_ribeiro2@hotmail.com>
2022-12-12 21:49:15 -0300

    Criando componente card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <div className='content'></div>
+        
+        <div className='content container mb-3 mt-3'>
+            <div className="row">
+              <div className="col-3">
+                <Card />
+              </div>
+              <div className="col-3">
+                <Card />
+              </div>
+              <div className="col-3">
+                <Card />
+              </div>
+              <div className="col-3">
+                <Card />
+              </div>
+              <div className="col-3">
+                <Card />
+              </div>
+            </div>
+        </div>
 
       <Footer />
     </div>
