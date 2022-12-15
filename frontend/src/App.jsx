eb5ddce966538f26fb7bc2fddc3de7507a780976
dielito2010 <d.a_ribeiro2@hotmail.com>
2022-12-15 20:04:31 -0300

    Importando route e routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Card from './components/Card/Card'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<p>Lista de cards</p>} />
+        <Route path="/cadastro" element={<p>Cadastro de itens</p>} />
+      </Routes>
         
         <div className='content container mb-3 mt-3'>
             <div className="row">
