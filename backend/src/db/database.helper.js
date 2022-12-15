1a2b2eefd1f7b96f9726f4425e39e747341fe763
dielito2010 <d.a_ribeiro2@hotmail.com>
2022-12-15 18:07:22 -0400

    setando banco na nuvem como padrão

--- backend/src/db/database.helper.js
+++ backend/src/db/database.helper.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-// const databaseUrl =
-//   "mongodb+srv://admin:V90K7ehx2krw7OlM@cluster0.gbnr4oi.mongodb.net/jornada-fullstack-avancado";
-const databaseUrl = "mongodb://127.0.0.1:27017/jornada-fullstack-avancado";
+const databaseUrl =
+  'mongodb+srv://admin:YzOCE9cR7TOWpyCV@cluster0.nxhs41w.mongodb.net/jornada-fullstack-avancado';
+//const databaseUrl = "mongodb://127.0.0.1:27017/jornada-fullstack-avancado";
 
 const connectToDatabase = () => {
   return mongoose
     .connect(databaseUrl)
-    .then(() => console.log("Banco de dados conectado com sucesso!"))
+    .then(() => console.log('Banco de dados conectado com sucesso!'))
     .catch((error) =>
-      console.log("Erro na conexão com o banco de dados.\n", error)
+      console.log('Erro na conexão com o banco de dados.\n', error)
     );
 };
 
