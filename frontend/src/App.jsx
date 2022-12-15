97ecf2e2b966693157ef4589c76b27698df87cc8
dielito2010 <d.a_ribeiro2@hotmail.com>
2022-12-15 20:16:31 -0300

    criando componente ItemList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import Card from './components/Card/Card'
-import { Routes, Route } from "react-router-dom"
+import ItemList from './components/ItemList/ItemList'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -10,29 +10,9 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<p>Lista de cards</p>} />
-        <Route path="/cadastro" element={<p>Cadastro de itens</p>} />
+        <Route path="/" element={<ItemList />}></Route>
+        <Route path="/cadastro" element={<p>Cadastro de itens</p>}></Route>
       </Routes>
-        
-        <div className='content container mb-3 mt-3'>
-            <div className="row">
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-            </div>
-        </div>
 
       <Footer />
     </div>
